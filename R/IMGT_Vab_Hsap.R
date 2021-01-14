c99369fbd01fb2ee0c1550e71f27e101a1eb585a
JulianSpagnuolo <JulianSpagnuolo@users.noreply.github.com>
2021-01-14 17:04:24 +0100

    added man entry for IMGT_Vab_Hsap data

---
+++ R/IMGT_Vab_Hsap.R
@@ -0,0 +1,20 @@
+#' Homo sapiens V-alpha and V-beta sequences from IMGT
+#'
+#' 
+#'
+#' @docType data
+#'
+#' @usage data(IMGT_Vab_Hsap)
+#'
+#' @format An object of class \code{"data.frame"}.
+#'
+#' @keywords datasets
+#'
+#'
+#'
+#' @examples
+#' data(IMGT_Vab_Hsap)
+#' \donttest{
+#' 
+#' }
+"ab.hsap"
