17a29e76709bb66b02acb20e0d143f2bbaea9d8a
JS <julian.spagnuolo@unibas.ch>
2021-02-02 14:28:35 +0100

    merged functions

--- src/Vdist.cpp
+++ src/Vdist.cpp
@@ -37,3 +37,30 @@ Rcpp::IntegerMatrix Vdist(std::vector<std::string> cdrs, Rcpp::StringVector Vgen
   
   return distmat;
 }
+
+//' @title TCRdist
+//' @name TCRdist
+//' @author Julian Spagnuolo
+//' @description 
+//'
+//' @param 
+//' @export
+// [[Rcpp::export]]
+IntegerMatrix CDRdistmat(std::vector<std::string> cdrs, Rcpp::StringVector clones, int cdr)
+{
+  
+  IntegerMatrix distmat(cdrs.size(), cdrs.size());
+  rownames(distmat) = clones;
+  colnames(distmat) = clones;
+  
+  /// put time saving method of calculating only half of the matrix back in!!!! and figure out how to return a dist class object!
+  for(int y = 0; y < cdrs.size(); y++)
+  {
+    for(int x = 0; x < cdrs.size(); x++)
+    {
+      distmat(x,y) = CDRdist(cdrs[x], cdrs[y], cdr);
+    }
+  }
+  
+  return distmat;
+}
